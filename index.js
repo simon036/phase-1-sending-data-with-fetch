6dd9a1a4dd3bd931c2e9d2fa7a47426c95045e64
Simon <ndungusimon564@gmail.com>
2024-12-13 15:38:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+const submitData = (name, email) => {
+    const url = "http://localhost:3000/users";
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+    const body = JSON.stringify({ 
+        name, email 
+    });
+    const request = fetch(url, {
+        method: 'POST',
+        headers, 
+        body 
+    });
+    return request
+    .then(response => {
+    return response.json();
+    })
+      .then(data => {
+        const id = data.id;
+        //Appending the new id to the DOM
+        const element = document.createElement("div");
+        element.textContent = `New user ID: ${id}`;
+        document.body.appendChild(element);
+      })
+      .catch(error => {
+        //Appending the error message to the DOM
+        const errorMessageElement = document.createElement("div");
+        errorMessageElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorMessageElement);
+      });
+};
